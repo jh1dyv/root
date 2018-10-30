cef2f8b4de228d26ab591bba3558d45fcd4017bb
DailyBuild <kanehori@haselab.net>
2018-10-30 16:25:33 +0900

    1030

--- SprManual/conv.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-if "%1" equ "" goto :end
-echo %1
-type %1 | sed -e "s/{sourcecode}/{sourcecode}/g" | sed -e "s/\([^\']\)zw/\1\\zw/g" | sed -e "s/^%%iflwarp(\(.*\))/\1/g" | nkf -w > tmp\%1
-shift
-:end
-exit /b
