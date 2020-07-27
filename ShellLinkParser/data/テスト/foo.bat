d01ca074cacef7d4547c5586fcfb02741cf9db80
jh1dyv <jh1dyv@gmail.com>
2020-07-27 13:10:38 +0900

    2020-0727-5

--- ShellLinkParser/data/テスト/foo.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo ƒeƒXƒg
-exit /b
--- ShellLinkParser/typedefs.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef	TYPEDEFS_H
-#define	TYPEDEFS_H
-#endif
-
-typedef unsigned int uint;
-typedef unsigned char byte;
-typedef unsigned short word;
-
-// end: typedefs.h
